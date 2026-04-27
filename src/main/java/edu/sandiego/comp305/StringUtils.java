001e845566978872acc62393f26a575ddecf440a
Isaac <fajardoisaac21@gmail.com>
2026-04-26 23:19:03 -0700

    fix: Build works added spaced befor @Test

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -3,10 +3,20 @@ package edu.sandiego.comp305;
 public final class StringUtils {
     private StringUtils() {}
 
+    /**
+     *
+     * @param input the string to reverse
+     * @return the reversed string
+     */
     public static String reverseString(final String input) {
 
         return new StringBuilder(input).reverse().toString();
     }
+
+    /**
+     *
+     * @param args CLI
+     */
 
     public static void main(final String[] args) {
         System.out.println(reverseString("Hello World!"));
--- src/test/java/edu/sandiego/comp305/StringUtilsTest.java
+++ src/test/java/edu/sandiego/comp305/StringUtilsTest.java
@@ -6,15 +6,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringUtilsTest {
+
     @Test
     public void testIsEmpty() {
         assertEquals("", StringUtils.reverseString(""));
 
     }
+
     @Test
     public void testReverseSimpleString() {
         assertEquals("olleh", StringUtils.reverseString("hello"));
     }
+
     @Test
     public void testReverseSingleCharacter() {
         assertEquals("a", StringUtils.reverseString("a"));
